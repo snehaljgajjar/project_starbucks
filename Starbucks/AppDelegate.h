5735c8467b9bdb84881d6d87f9f588ac455cf17e
Snehal Gajjar <sgajjar@pradips-macbook-pro.local>
2017-07-24 22:25:56 -0700

    Initial Commit

---
+++ Starbucks/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Starbucks
+//
+//  Created by Snehal Gajjar on 7/24/17.
+//  Copyright © 2017 Snehal Gajjar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Starbucks/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Starbucks
+//
+//  Created by Snehal Gajjar on 7/24/17.
+//  Copyright © 2017 Snehal Gajjar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
